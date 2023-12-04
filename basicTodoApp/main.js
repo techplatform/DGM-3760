522def44a9d13017aa2e8630257d9372bf7cae9a
techplatform <10854265@uvu.edu>
2023-12-03 22:42:14 -0700

    BasicToDoAPP

---
+++ basicTodoApp/main.js
@@ -0,0 +1,56 @@
+const toDoList = document.getElementById("toDoList");
+const newToDoInput = document.getElementById("newToDo");
+const todoRemainingSpan = document.getElementById("todoRemaining");
+
+function addToDo() {
+  const todoText = newToDoInput.value.trim();
+  if (todoText === "") return;
+
+  const todoItem = document.createElement("li");
+  todoItem.innerHTML = `
+            <input type="checkbox" onchange="updateTodosLeft()">
+            <span>${todoText}</span>
+            <button onclick="editTodo(this)">Edit</button>
+            <button onclick="deleteTodo(this)">Delete</button>
+        `;
+
+  toDoList.appendChild(todoItem);
+  newToDoInput.value = "";
+  updateTodosLeft();
+}
+
+function editTodo(button) {
+  const todoItem = button.parentElement;
+  const todoText = todoItem.querySelector("span");
+  const newText = prompt("Edit todo:", todoText.innerText);
+  if (newText !== null) {
+    todoText.innerText = newText;
+  }
+}
+
+function deleteTodo(button) {
+  const todoItem = button.parentElement;
+  toDoList.removeChild(todoItem);
+  updateTodosLeft();
+}
+
+function updateTodosLeft() {
+  const checkboxes = document.querySelectorAll(
+    '#toDoList input[type="checkbox"]'
+  );
+  const todoRemaining = Array.from(checkboxes).filter(
+    (checkbox) => !checkbox.checked
+  ).length;
+  todoRemainingSpan.innerText = todoRemaining;
+}
+
+function clearCompleted() {
+  const completedTodos = document.querySelectorAll(
+    '#toDoList input[type="checkbox"]:checked'
+  );
+  completedTodos.forEach((checkbox) => {
+    const todoItem = checkbox.parentElement;
+    toDoList.removeChild(todoItem);
+  });
+  updateTodosLeft();
+}
