4044eed2e2e95c33f702bfa3a103057ab24d0779
techplatform <10854265@uvu.edu>
2023-12-04 00:33:00 -0700

    backEnd

---
+++ backEnd/app.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const path = require('path'); //helping find the file path
+
+const app = express();
+const PORT = 3001; //port number - can be changed
+
+app.use(bodyParser.json());
+
+let todos = [
+  {
+    id: 1,
+    text: "Get Eggs",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "Buy Lotion",
+    completed: true,
+  },
+  {
+    id: 3,
+    text: "Call Grandma",
+    completed: false,
+  },
+];
+let categories = [
+  { id: 1, name: "Work" },
+  { id: 2, name: "School" },
+  { id: 3, name: "Shopping" },
+];
+
+
+//calling my main html page from here
+app.use(express.static('public'));
+
+//getting the todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+//post
+app.post("/todos", (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+//put
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedTodo = req.body;
+
+  todos = todos.map((todo) => (todo.id === id ? updatedTodo : todo));
+
+  res.json(updatedTodo);
+});
+
+//delete
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  todos = todos.filter((todo) => todo.id !== id);
+
+  res.json({ message: "Todo deleted" });
+});
+
+// category todos
+app.get("/todos/category/:category", (req, res) => {
+  const category = req.params.category;
+  const categoryTodos = todos.filter((todo) => todo.category === category);
+
+  res.json(categoryTodos);
+});
+
+//get categories
+app.get("/categories", (req, res) => {
+  res.json(categories);
+});
+
+//post categorie
+app.post("/categories", (req, res) => {
+  const newCategory = req.body;
+  categories.push(newCategory);
+  res.json(newCategory);
+});
+
+//put categories
+app.put("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedCategory = req.body;
+
+  categories = categories.map((category) =>
+    category.id === id ? updatedCategory : category
+  );
+
+  res.json(updatedCategory);
+});
+
+//delete catagories
+app.delete("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  categories = categories.filter((category) => category.id !== id);
+
+  res.json({ message: "Category deleted" });
+});
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+//server point
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
---
+++ backEnd/public/main.js
@@ -0,0 +1,123 @@
+const toDoList = document.getElementById("toDoList");
+const newToDoInput = document.getElementById("newToDo");
+const categorySelect = document.getElementById('categorySelect');
+const categoryList = document.getElementById('categoryList');
+const newCategoryInput = document.getElementById('newCategory');
+const todoRemainingSpan = document.getElementById("todoRemaining");
+
+function addToDo() {
+  const todoText = newToDoInput.value.trim();
+    const selectedCategory = categorySelect.value;
+
+    if (todoText === '' || selectedCategory === 'default') return;
+
+    const todoItem = document.createElement('li');
+    todoItem.setAttribute('data-category', selectedCategory);
+    todoItem.innerHTML = `
+        <input type="checkbox" onchange="updateTodoRemaining()">
+        <span>${todoText}</span>
+        <span class="categoryName">(${selectedCategory})</span>
+        <button onclick="editToDo(this)">Edit</button>
+        <button onclick="deleteToDo(this)">Delete</button>
+    `;
+
+    toDoList.appendChild(todoItem);
+    newToDoInput.value = '';
+    updateTodoRemaining();
+}
+
+function editToDo(button) {
+  const todoItem = button.parentElement;
+  const todoText = todoItem.querySelector("span");
+  const newText = prompt("Edit todo:", todoText.innerText);
+  if (newText !== null) {
+    todoText.innerText = newText;
+  }
+}
+
+function deleteToDo(button) {
+  const todoItem = button.parentElement;
+  toDoList.removeChild(todoItem);
+  updateTodoRemaining();
+}
+
+function updateTodoRemaining() {
+  const checkboxes = document.querySelectorAll(
+    '#toDoList input[type="checkbox"]'
+  );
+  const todoRemaining = Array.from(checkboxes).filter(
+    (checkbox) => !checkbox.checked
+  ).length;
+  todoRemainingSpan.innerText = todoRemaining;
+}
+
+function clearCompleted() {
+  const completedTodos = document.querySelectorAll(
+    '#toDoList input[type="checkbox"]:checked'
+  );
+  completedTodos.forEach((checkbox) => {
+    const todoItem = checkbox.parentElement;
+    toDoList.removeChild(todoItem);
+  });
+  updateTodoRemaining();
+}
+
+function addCategory() {
+    const newCategoryText = newCategoryInput.value.trim();
+    if (newCategoryText === '') return;
+
+    const categoryItem = document.createElement('li');
+    categoryItem.innerHTML = `
+        ${newCategoryText}
+        <button onclick="editCategory(this)">Edit</button>
+        <button onclick="deleteCategory(this)">Delete</button>
+    `;
+
+    categoryList.appendChild(categoryItem);
+    newCategoryInput.value = '';
+    updateCategorySelect();
+}
+
+function editCategory(button) {
+    const categoryItem = button.parentElement;
+    const categoryText = categoryItem.firstChild;
+    const newText = prompt('Edit category:', categoryText.nodeValue);
+    if (newText !== null) {
+        categoryText.nodeValue = newText;
+        updateCategorySelect();
+    }
+}
+
+function deleteCategory(button) {
+    const categoryItem = button.parentElement;
+    categoryList.removeChild(categoryItem);
+    updateCategorySelect();
+}
+
+function updateCategorySelect() {
+    // Clear existing options
+    categorySelect.innerHTML = '<option value="default">Select Category</option>';
+
+    // Populate category options from the categoryList
+    const categories = categoryList.getElementsByTagName('li');
+    for (const category of categories) {
+        const categoryName = category.firstChild.nodeValue;
+        const option = document.createElement('option');
+        option.value = categoryName;
+        option.textContent = categoryName;
+        categorySelect.appendChild(option);
+    }
+}
+
+function updateCategory() {
+    const selectedCategory = categorySelect.value;
+
+    // Highlight selected category in the categoryList
+    const categories = categoryList.getElementsByTagName('li');
+    for (const category of categories) {
+        category.classList.remove('selected-category');
+        if (category.firstChild.nodeValue === selectedCategory) {
+            category.classList.add('selected-category');
+        }
+    }
+}
